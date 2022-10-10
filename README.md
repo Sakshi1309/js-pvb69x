de4b38bbcfbaddab0a8457eefaa15537a3d5c94d
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2022-10-10 13:17:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-pvb69x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pvb69x)
---
+++ index.js
@@ -0,0 +1,32 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function x() {
+  var a = 1;
+  return function y() {
+    var b = 2;
+    return function z() {
+      console.log(a, b);
+    };
+  };
+}
+x()()();
+
+function xyz() {
+  for (var i = 1; i <= 5; i++) {
+    function abc(i) {
+      setTimeout(() => {
+        console.log(i);
+      }, i * 1000);
+    }
+    abc(i);
+  }
+}
+
+xyz();
+
+// Functions bundled together along with its outer lexical scope is know as clouser. This means whenever a function is returned, even if it is vanished in execution context it still remembers the reference it was pointing to. 
