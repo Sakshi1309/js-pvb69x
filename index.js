b27ac1b052f7a794d0abced3dceb4fd4eca94d61
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2022-12-20 15:06:04 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-
-
-
-
 let hero = {
   powerLevel: 99,
   getPower() {
@@ -21,3 +17,41 @@ let getPower = hero.getPower;
 let hero2 = { powerLevel: 42 };
 console.log(getPower.apply(hero));
 console.log(getPower.apply(hero2));
+
+var employees = [
+  {
+    id: 20,
+    name: 'Ajay',
+    salary: 30000,
+    dept: 'it',
+  },
+  {
+    id: 24,
+    name: 'Vijay',
+    salary: 35000,
+    dept: 'hr',
+  },
+  {
+    id: 20,
+    name: 'Rahul',
+    salary: 32000,
+    dept: 'it',
+  },
+  {
+    id: 88,
+    name: 'Raman',
+    salary: 38000,
+    dept: 'hr',
+  },
+];
+var departmentList = employees.filter(function (record, index) {
+  let dup = employees.find((i) => i.id === index);
+  console.log(dup);
+
+  return record;
+});
+console.log(departmentList);
+
+var mapArr = employees.map((item) => {
+  return { name: item.name, salary: item.salary };
+});
