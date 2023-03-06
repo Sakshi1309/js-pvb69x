714b1a4874ff097ef0d5eb06c81a90f5e40152ed
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2023-03-06 14:05:36 +0530

    1 file modified

--- map/set.js
+++ map/set.js
@@ -16,3 +16,17 @@ let obj1 = {
 };
 
 const newSet3 = new WeakSet([obj1]);
+
+const ws = new WeakSet();
+const foo = {};
+const bar = {};
+
+ws.add(foo);
+ws.add(bar);
+
+ws.has(foo); // true
+ws.has(bar); // true
+
+ws.delete(foo); // removes foo from the set
+ws.has(foo); // false, foo has been removed
+ws.has(bar); // true, bar is retained
