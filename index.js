273ff33a4e13ebddf5d695346d5ead68fdb469b8
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2023-03-15 18:19:36 +0530

    2 files modified

--- index.js
+++ index.js
@@ -96,3 +96,16 @@ console.log(filArr);
 //   }
 // }, []);
 // console.log(filteredArr);
+
+let function1 = () => {
+  console.log(this);
+};
+let function2 = function () {
+  console.log(this);
+};
+
+window.addEventListener('load', function1);
+window.addEventListener('load', function2);
+
+document.getElementById('btn1').addEventListener('click', function1);
+document.getElementById('btn2').addEventListener('click', function2);
