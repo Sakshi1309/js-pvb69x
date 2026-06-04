968519a4e8f04e8729d4a89899da502d93c2969c
Sakshi1309 <git@stackblitz.com>
2026-06-04 12:02:01 +0530

    1 file modified

--- interview.js
+++ interview.js
@@ -64,68 +64,65 @@
 
 // --------------------------------------
 
-function secondLargest(arr) {
-  let largest = 0;
-  let secondLargest = 0;
-  for (let num of arr) {
-    if (num > largest) {
-      secondLargest = largest;
-      largest = num;
-    } else if (num > secondLargest && num < largest) {
-      secondLargest = num;
-    }
-  }
-  return secondLargest;
-}
+// function secondLargest(arr) {
+//   let largest = 0;
+//   let secondLargest = 0;
+//   for (let num of arr) {
+//     if (num > largest) {
+//       secondLargest = largest;
+//       largest = num;
+//     } else if (num > secondLargest && num < largest) {
+//       secondLargest = num;
+//     }
+//   }
+//   return secondLargest;
+// }
 
-console.log(secondLargest([10, 20, 4, 45, 99, 99]));
+// console.log(secondLargest([10, 20, 4, 45, 99, 99]));
 
-let arr = [10, 20, 4, 45, 99, 99];
-let newArr = [...new Set(arr)];
-newArr.sort((a, b) => b - a);
-console.log(newArr[1]);
+// let arr = [10, 20, 4, 45, 99, 99];
+// let newArr = [...new Set(arr)];
+// newArr.sort((a, b) => b - a);
+// console.log(newArr[1]);
 
 // -------------------------------------------------
 
-// const multiDimArray = [1, [2, 3, [4, 5]], [6, [7, 8, [9, 10]], 11], 12];
-// //output: [ 1, 2, 3, 4,  5, 6,  7, 8, 9, 10, 11, 12]
-// function flattenArray(arr) {
-//   let result = [];
-//   arr.forEach((item) => {
-//     console.log(item, result);
-//     if (Array.isArray(item)) {
-//       result = result.concat(flattenArray(item));
-//       console.log('2', item, result);
-//     } else {
-//       result.push(item);
-//       console.log('3', item, result);
-//     }
-//   });
-//   return result;
-// }
-// const flattenedArray = flattenArray(multiDimArray);
-// console.log('Try programiz.pro', flattenedArray);
+const multiDimArray = [1, [2, 3, [4, 5]], [6, [7, 8, [9, 10]], 11], 12];
+// output: [ 1, 2, 3, 4,  5, 6,  7, 8, 9, 10, 11, 12]
+function flattenArray(arr) {
+  let result = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+console.log('Try programiz.pro', flattenArray(multiDimArray));
 
 // ------------------------------------------
 
-// let arr1 = [
-//   { name: 'A', age: 34 },
-//   { name: 'B', age: 35 },
-//   { name: 'A', age: 67 },
-//   { name: 'ABC', age: 34 },
-//   { name: 'ABC', age: 344 },
-//   { name: 'ABC', age: 36 },
-// ];
-// // output :- {A: 2, B: 1, ABC: 3}
-// const count = {};
-// arr1.forEach((item) => {
-//   if (count[item.name]) {
-//     count[item.name] += 1;
-//   } else {
-//     count[item.name] = 1;
-//   }
-// });
-// console.log('count obj', count);
+let arr1 = [
+  { name: 'A', age: 34 },
+  { name: 'B', age: 35 },
+  { name: 'A', age: 67 },
+  { name: 'ABC', age: 34 },
+  { name: 'ABC', age: 344 },
+  { name: 'ABC', age: 36 },
+];
+// output :- {A: 2, B: 1, ABC: 3}
+const count = {};
+arr1.forEach((item) => {
+  if (count[item.name]) {
+    count[item.name] += 1;
+  } else {
+    count[item.name] = 1;
+  }
+});
+console.log('count obj', count);
 
 // --------------------------------------------
 
